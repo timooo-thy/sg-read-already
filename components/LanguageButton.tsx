317c52569f2bac687fac9870a2e6061e1c41ee60
Timothy Lee <timothylhy@hotmail.com>
2024-02-05 00:07:07 +0800

    default lang to be english

--- components/LanguageButton.tsx
+++ components/LanguageButton.tsx
@@ -39,7 +39,7 @@ interface LanguageButtonProps {
 
 export function LanguageButton({ onLanguageChange }: LanguageButtonProps) {
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("");
+  const [value, setValue] = React.useState("en");
 
   const handleSelect = (currentValue: Language) => {
     setValue(currentValue);
